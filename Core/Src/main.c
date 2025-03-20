c24e628c62f26f9477b22b4dd31aac74b374a9fe
dd060606 <dd06.developpement@gmail.com>
2025-03-20 19:01:41 +0100

    new tests

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -102,7 +102,7 @@ int main(void)
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
   HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
-
+  //HAL_TIM_Encoder_Start_IT(&htim2, TIM_CHANNEL_ALL);
   ModelecOdometrySetup();
   /* USER CODE END 2 */
 
